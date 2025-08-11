923614a73ce484d905189a905e8a7cda99b8cd6c
youcefshaaban <yousafsh351@gmail.com>
2025-08-11 14:32:58 +0300

    Improved Script

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ import random
 import string
 
 print("======== Password Generator ========")
-symbols = string.ascii_letters + string.digits + string.punctuation
-length = int(input("How long should the password be? > "))
-password = ""
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
 
-for i in range(length):
-  password += random.choice(symbols)
+letters_length = int(input("How man letters do you want? > "))
+numbers_length = int(input("How man digits do you want? > "))
+symbols_length = int(input("How man symbols do you want? > "))
+
+# Picking All Characters depending on how long the user wants
+password_chars = (
+  [random.choice(letters) for _ in range(letters_length)] +
+  [random.choice(numbers) for _ in range(numbers_length)] +
+  [random.choice(symbols) for _ in range(symbols_length)] 
+)
+
+# Asking the user for the password length
+passowrd_length = int(input("How long should the password be? > "))
+
+
+all_symbols = letters + numbers + symbols
+remaining_chars = passowrd_length - len(password_chars)
+
+# Conecatinating Characters with the password_chars
+password_chars += [random.choice(all_symbols) for _ in range(max(remaining_chars, 0))]
+
+# Shuffle the password characters to be fully randomized
+random.shuffle(password_chars)
+
+# Then join all characters together in a final result
+password = ''.join(password_chars)
 
 print("Your Password is:")
 print(password)
